bafb30a53712aa2749cc0e72cdb5dd8bfad4ac42
FullOvellas <sergiotui1c@gmail.com>
2023-05-15 10:58:25 +0200

    Initial commit

---
+++ src/main/java/com/imatia/campusdual/grupoun_bootcampbackend/GrupoUnBootcampBackendApplication.java
@@ -0,0 +1,13 @@
+package com.imatia.campusdual.grupoun_bootcampbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrupoUnBootcampBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrupoUnBootcampBackendApplication.class, args);
+    }
+
+}
