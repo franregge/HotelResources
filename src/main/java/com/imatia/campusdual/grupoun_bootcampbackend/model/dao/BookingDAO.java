b2aa32cc6dbf7af96d388586dd838954df0cffc2
Sergio Alonso Pazo <sergiotui1c@gmail.com>
2023-05-22 14:09:51 +0200

    Update src/main/java/com/imatia/campusdual/grupoun_bootcampbackend/model/dao/BookingDAO.java
    Co-authored-by: cesarsantiagonunez <86664126+cesarsantiagonunez@users.noreply.github.com>

--- src/main/java/com/imatia/campusdual/grupoun_bootcampbackend/model/dao/BookingDAO.java
+++ src/main/java/com/imatia/campusdual/grupoun_bootcampbackend/model/dao/BookingDAO.java
@@ -3,5 +3,6 @@ package com.imatia.campusdual.grupoun_bootcampbackend.model.dao;
 import com.imatia.campusdual.grupoun_bootcampbackend.model.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface BookingDAO extends JpaRepository<Booking,Integer> {
 }
