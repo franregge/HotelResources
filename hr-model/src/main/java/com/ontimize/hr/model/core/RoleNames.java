b164953c34d6bd66b1d8af4150e778716b4268e0
franregge <franregueira89@gmail.com>
2023-07-14 11:26:26 +0200

    TestFixing

--- hr-model/src/main/java/com/ontimize/hr/model/core/RoleNames.java
+++ hr-model/src/main/java/com/ontimize/hr/model/core/RoleNames.java
@@ -2,13 +2,12 @@ package com.ontimize.hr.model.core;
 
 public class RoleNames {
 
-    private RoleNames() {
-    }
 
     public static final String MANAGER = "manager";
     public static final String ROOT = "root";
     public static final String CLIENT = "client";
     public static final String GUEST = "guest";
     public static final String EMPLOYEE = "employee";
+    public static final String RECEPTIONIST = "receptionist";
 
 }
